0b346ed99b203c4ec39e2c6b7a6e172d4e841db5
Logan Abramowitz <loganabramowitz@gmail.com>
2023-01-19 15:14:50 -0500

    Comments

--- app/src/main/java/com/example/qrscanner/MainActivity.java
+++ app/src/main/java/com/example/qrscanner/MainActivity.java
@@ -36,6 +36,7 @@ import es.rcti.printerplus.printcom.models.StructReport;
 
 public class MainActivity extends AppCompatActivity {
 
+    //initializing variables that are used in the project
     Button btn_scan;
     Button btn_print;
     private TextView firstName;
@@ -50,10 +51,10 @@ public class MainActivity extends AppCompatActivity {
     private ProgressDialog progressDialog;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //This is the main function that runs the code
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        btn_scan = findViewById(R.id.btn_scan);
+        setContentView(R.layout.activity_main); //This is the layout (xml file) that is showing at the beginning
+        btn_scan = findViewById(R.id.btn_scan); //This is how you connect parts from the layout to variables
         btn_print = findViewById(R.id.btn_print);
 
         firstName = findViewById(R.id.firstNameTextView);
@@ -64,10 +65,10 @@ public class MainActivity extends AppCompatActivity {
         progressDialog = new ProgressDialog(MainActivity.this);
         progressDialog.setMessage("Loading...");
 
-
+        //This is a function that waits for a button to be clicked
         btn_scan.setOnClickListener(v->
         {
-            msr = new StructReport();
+            msr = new StructReport(); //generates a report to be printed with printer plus app - makes use of printcom.aar in libs folder
             msr.addItemAlignment( StructReport.ALIGNMENT_CENTER );
             msr.addTextBold(false);
             msr.addTextUnderlined(false);
@@ -81,24 +82,24 @@ public class MainActivity extends AppCompatActivity {
                     + "\nGrade: " + grade.getText());
             addData();
             progressDialog.show();
-            PrintTool.sendOrder(MainActivity.this, msr);
+            PrintTool.sendOrder(MainActivity.this, msr); //sends the report to the printer - also uses printcom.aar
 
         });
 
     }
-    private void scanCode(){
+    private void scanCode(){ //this scans the qr code
         ScanOptions options = new ScanOptions();
         options.setBeepEnabled(true);
         options.setOrientationLocked(true);
         options.setCaptureActivity(CaptureAct.class);
-        barLauncher.launch(options);
+        barLauncher.launch(options); //this launches the camera and prepares it to scan the qr code
     }
 
     ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result ->
     {
         if (result.getContents() != null){
 
-            try {
+            try { //attempts to extract data from qr code
                 obj = new JSONObject(result.getContents());
                 firstName.setText((String) obj.get("First Name"));
                 lastName.setText((String)obj.get("Last Name"));
@@ -111,12 +112,13 @@ public class MainActivity extends AppCompatActivity {
         }
     });
 
-    public void addData(){
+    public void addData(){ //this sends data to the spread sheet - requires volley import and gradle implementation
         String sFirstName = firstName.getText().toString();
         String sLastName = lastName.getText().toString();
         String sEmail = email.getText().toString();
         String sGrade = grade.getText().toString();
 
+        //the url is from when you deploy the script for the spread sheet. The script code is in the helper scripts folder of the github
         StringRequest stringRequest = new StringRequest(Request.Method.POST, "https://script.google.com/macros/s/AKfycbwdZ-FD7fRD3St-Kyzd4tksafEb0WXBi_12pROsT1YJvzw8E-3FAV5oyCad2dsSzZa-cA/exec", new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -131,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }){
             @Override
-            protected Map<String, String> getParams(){
+            protected Map<String, String> getParams(){ //maps keywords to data values to be put into the spreadsheet
                 Map<String,String> params = new HashMap<>();
                 params.put("action","addStudent");
                 params.put("vFirstName",sFirstName);
